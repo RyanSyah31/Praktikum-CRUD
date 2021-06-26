1eb456009b7d3442c95b5abbf126e8b7e07dfe25
RyanSyah31 <46744986+RyanSyah31@users.noreply.github.com>
2021-06-26 16:51:48 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Praktikum-CRUD
+# MEMBUAT_CRUD
+
+<img src="CRUD.png" alt="Girl in a jacket" width="852" height="480">
---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$db_name = 'ryan_syah_311810264'; // nama databasenya
+$conn = new mysqli($host, $username, $password, $db_name);
+?>
